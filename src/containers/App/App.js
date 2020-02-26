c378e2caacb54ca271b849cfc43d6dad9bc97571
Carla Geglio <carla.geglio@gmail.com>
2020-02-25 19:07:02 -0700

    Add basic setup and route to app

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Route, Redirect } from 'react-router-dom';
 import './App.scss';
+import Form from '../Form/Form'
+import Nav from '../Nav/Nav'
+import Loader from '../../components/Loader/Loader'
+import CardContainer from '../CardContainer/CardContainer'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+export class App extends Component {
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <main>
+        <Route exact path="/">
+          {this.props.user.username ? <Redirect to='/home' /> :
+            <section className='login-page'>
+              <Form />
+            </section>
+          }
+        </Route>
+        <Route exact path='/home'>
+            <Nav />
+            {!this.props.cards.length && <Loader />}
+            <CardContainer />
+        </Route>
+      </main>
+    )
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  user: state.user,
+  cards: state.cards
+})
+
+export default connect(mapStateToProps)(App);
