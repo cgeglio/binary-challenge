ea831fbcb4edbdcc8707531e911087365d3cf54c
Carla Geglio <carla.geglio@gmail.com>
2020-02-26 19:52:27 -0700

    Remove console log

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Card.scss';
 
 export const Card = ({ card }) => {
-  console.log(card)
   return (
     <article>
       <img src={process.env.PUBLIC_URL + `/cardAssets/${card.name_short}.jpg`} alt="Tarot card" />
