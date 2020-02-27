b50f450523a0ff66a5958c9dbeac753ceb3a0831
Carla Geglio <carla.geglio@gmail.com>
2020-02-26 19:44:53 -0700

    Add card details that show on hover

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,9 +2,19 @@ import React from 'react';
 import './Card.scss';
 
 export const Card = ({ card }) => {
+  console.log(card)
   return (
     <article>
       <img src={process.env.PUBLIC_URL + `/cardAssets/${card.name_short}.jpg`} alt="Tarot card" />
+      <div className="overlay">
+        <div className="card-details">
+          <h3>{card.name}</h3>
+          <h4>Meaning:</h4>
+          <p>{card.meaning_up}</p>
+          <h4>Description:</h4>
+          <p>{card.desc}</p>
+        </div>
+      </div>
     </article>
   )
 }
