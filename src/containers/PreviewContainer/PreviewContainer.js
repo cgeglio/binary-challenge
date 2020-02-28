daa74a4ad27789085dfe2d90622a28f7da64402a
Carla Geglio <carla.geglio@gmail.com>
2020-02-28 10:35:51 -0700

    Add back button to preview container

--- src/containers/PreviewContainer/PreviewContainer.js
+++ src/containers/PreviewContainer/PreviewContainer.js
@@ -1,23 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './PreviewContainer.scss';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { removeQuestion } from '../../actions';
 import ReadingPreview from '../../components/ReadingPreview/ReadingPreview';
 
-export const PreviewContainer = (props) => {
-  return (
-    <section className='preview-container'>
-      <h2 className='saved-title'>Saved Readings</h2>
-      {!props.favorites.length ? <h2 className='saved-error'>You have not saved any readings yet.</h2> :
-      props.favorites.map(favorite => {
-        return <ReadingPreview key={favorite.id} reading={favorite} />
-      })
-      }
-    </section>
-  )
+export class PreviewContainer extends Component {
+
+  resetQuestion = () => {
+    this.props.resetQuestionInStore(this.props.question);
+  }
+
+  render() {
+    return (
+      <section className='preview-container'>
+        <h2 className='saved-title'>Saved Readings</h2>
+        {!this.props.favorites.length ? <h2 className='saved-error'>You have not saved any readings yet.</h2> :
+        this.props.favorites.map(favorite => {
+          return <ReadingPreview key={favorite.id} reading={favorite} />
+        })
+        }
+        <Link to='/home'><button onClick={() => this.resetQuestion()} className='back-btn'>Home</button></Link>
+      </section>
+    )
+  }
 }
 
 export const mapStateToProps = state => ({
-  favorites: state.favorites
+  favorites: state.favorites,
+  question: state.question,
 })
 
-export default connect(mapStateToProps)(PreviewContainer)
+export const mapDispatchToProps = dispatch => ({
+  resetQuestionInStore: question => (dispatch(removeQuestion(question)))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PreviewContainer)
