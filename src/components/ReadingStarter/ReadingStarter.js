edb5b6feccfe05ea5087c61ed017b5c17447b1d7
Carla Geglio <carla.geglio@gmail.com>
2020-02-28 12:53:28 -0700

    Update snapshots

--- src/components/ReadingStarter/ReadingStarter.js
+++ src/components/ReadingStarter/ReadingStarter.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import './ReadingStarter.scss';
 import { connect } from 'react-redux';
 import { addQuestion } from '../../actions'
