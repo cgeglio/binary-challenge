2fe7f751f3ce04e27cad55f8471a8257c7b08681
Carla Geglio <carla.geglio@gmail.com>
2020-03-01 14:23:57 -0700

    Remove actions and reducers for fortune and card

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,13 @@
 import { combineReducers } from 'redux';
 import { user } from './userReducer';
-import { cards } from './cardsReducer';
 import { question } from './questionReducer';
-import { fortune } from './fortuneReducer';
 import { currentReading } from './readingReducer';
 import { favorites } from './favoritesReducer';
 import { spreadNumber } from './spreadReducer';
 
 export const rootReducer = combineReducers({
   user,
-  cards,
   question,
-  fortune,
   currentReading,
   favorites,
   spreadNumber
