ecf3ae052f0749a1094dead65945e77eec5e3cb7
Carla Geglio <carla.geglio@gmail.com>
2020-02-29 19:51:00 -0700

    Add preview container test

--- src/containers/PreviewContainer/PreviewContainer.test.js
+++ src/containers/PreviewContainer/PreviewContainer.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { PreviewContainer, mapStateToProps } from './PreviewContainer';
+import { PreviewContainer, mapStateToProps, mapDispatchToProps } from './PreviewContainer';
+import { removeQuestion } from '../../actions';
 import { shallow } from 'enzyme';
 
 describe('PreviewContainer', () => {
@@ -13,24 +14,44 @@ describe('PreviewContainer', () => {
   });
 
   describe('mapStateToProps', () => {
-    it('should return an array of favorites', () => {
+    it('should return an array of favorites and a question', () => {
       const mockDispatch = jest.fn();
       const mockFavorites = [{cards: [{name: 'The Magician'}], id: 20}];
       const wrapper = shallow(<PreviewContainer favorites={mockFavorites} />);
       const mockState = {
         user: {user: {name: 'Bithcuits', id: 9} },
+        question: 'Is the sky blue?',
         favorites: [
           {cards: [{name: 'The Magician'}], id: 20},
           {cards: [{name: 'The Moon'}], id: 10}
         ]
       };
-      const expected = {favorites: [
-        {cards: [{name: 'The Magician'}], id: 20},
-        {cards: [{name: 'The Moon'}], id: 10}
+      const expected = {
+        question: 'Is the sky blue?',
+        favorites: [
+          {cards: [{name: 'The Magician'}], id: 20},
+          {cards: [{name: 'The Moon'}], id: 10}
       ]};
       const mappedProps = mapStateToProps(mockState);
       expect(mappedProps).toEqual(expected);
     })
+  });
+
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch with the removeQuestion action when resetQuestion is called', () => {
+      let mockResetQuestion = jest.fn().mockImplementation();
+      let mockFavorites = [
+        {cards: [{name: 'The Magician'}], id: 20},
+        {cards: [{name: 'The Moon'}], id: 10}
+      ];
+      const wrapper = shallow(<PreviewContainer favorites={mockFavorites} removeQuestion={mockResetQuestion} />);
+      const mockDispatch = jest.fn();
+      const actionToDispatch = removeQuestion('question');
+      const mappedProps = mapDispatchToProps(mockDispatch);
+
+      mappedProps.resetQuestion('question');
+      expect(mockDispatch).toHaveBeenCalledWith(actionToDispatch);
+    });
   });
 
 });
